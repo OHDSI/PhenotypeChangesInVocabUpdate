ef82a9eba9f2f2c2c1c89bebda07c0af231c16e0
Sadowski,Katy (MED MA) BIP-US-R <katy.sadowski@boehringer-ingelheim.com>
2024-12-09 20:04:26 +0000

    fixes to work in Snowflake

--- inst/sql/sql_server/AllFromNodes.sql
+++ inst/sql/sql_server/AllFromNodes.sql
@@ -4,9 +4,9 @@ select cohortid,cohortName, conceptsetname, conceptsetid, isexcluded, includedes
 cn.concept_id as Node_concept_id , cn.concept_name as node_concept_name , coalesce (aro.descendant_record_count, 0) as drc,
 cm.concept_id as maps_to_concept_id, cm.concept_name as maps_to_concept_name,
 cmv.concept_id as maps_to_value_concept_id, cmv.concept_name as maps_to_value_concept_name
-from #ConceptsInCohortSet s
+from @conceptTableName s
 join @newVocabSchema.concept cn on cn.concept_id = s.conceptid and cn.standard_concept is null
-left join @resultSchema.achilles_result_cc aro on aro.concept_id = cn.concept_id
+left join @resultSchema.achilles_result_concept_count aro on aro.concept_id = cn.concept_id
 left join @newVocabSchema.concept_relationship cr on cr.concept_id_1 = cn.concept_id and cr.relationship_id ='Maps to'
 left join @newVocabSchema.concept cm on cm.concept_id = cr.concept_id_2
 left join @newVocabSchema.concept_relationship crv on crv.concept_id_1 = cn.concept_id and crv.relationship_id ='Maps to value'
@@ -17,7 +17,7 @@ order by drc desc
 --old vocabulary vs new, source concept comparison
 create table #old_vc as
 select cohortid, cohortName,conceptsetname, conceptsetid, ca.descendant_concept_id
-from #ConceptsInCohortSet s
+from @conceptTableName s
 join @oldVocabSchema.concept cn on cn.concept_id = s.conceptid
 join @oldVocabSchema.concept_ancestor ca on ca.ancestor_concept_id = cn.concept_id
 and ((includedescendants = 0 and ca.ancestor_concept_id = ca.descendant_concept_id ) or includedescendants != 0)
@@ -26,7 +26,7 @@ and isexcluded = 0
 and s.conceptid not in (@excludedNodes)
 except
 select cohortid,cohortName, conceptsetname, conceptsetid , ca.descendant_concept_id
-from #ConceptsInCohortSet s
+from @conceptTableName s
 join @oldVocabSchema.concept cn on cn.concept_id = s.conceptid
 join @oldVocabSchema.concept_ancestor ca on ca.ancestor_concept_id = cn.concept_id
 and ((includedescendants = 0 and ca.ancestor_concept_id = ca.descendant_concept_id ) or includedescendants != 0)
@@ -35,7 +35,7 @@ and isexcluded = 1
 create table #new_vc
  as
 select cohortid, cohortName,conceptsetname, conceptsetid, ca.descendant_concept_id
-from #ConceptsInCohortSet s
+from @conceptTableName s
 join @newVocabSchema.concept cn on cn.concept_id = s.conceptid
 join @newVocabSchema.concept_ancestor ca on ca.ancestor_concept_id = cn.concept_id
 and ((includedescendants = 0 and ca.ancestor_concept_id = ca.descendant_concept_id ) or includedescendants != 0)
@@ -44,13 +44,13 @@ and isexcluded = 0
 and s.conceptid not in (@excludedNodes)
 except
 select cohortid,cohortName, conceptsetname, conceptsetid , ca.descendant_concept_id
-from #ConceptsInCohortSet s
+from @conceptTableName s
 join @newVocabSchema.concept cn on cn.concept_id = s.conceptid
 join @newVocabSchema.concept_ancestor ca on ca.ancestor_concept_id = cn.concept_id
 and ((includedescendants = 0 and ca.ancestor_concept_id = ca.descendant_concept_id ) or includedescendants != 0)
 and isexcluded = 1
 ;
-create table #resolv_dif_sc as 
+create table #resolv_dif_sc as
 with
 old_vc_map as (
 select cohortid,cohortName, conceptsetname, conceptsetid, r.concept_id_2 as source_concept_id
@@ -87,7 +87,7 @@ from #resolv_dif_sc dif
 join @newVocabSchema.concept cs on cs.concept_id = dif.source_concept_id -- to get source_concept_id info
 left join @oldVocabSchema.concept_relationship cr on cr.concept_id_1 = dif.source_concept_id and cr.relationship_id ='Maps to'
 left join @oldVocabSchema.concept c on c.concept_id = cr.concept_id_2
-join @resultSchema.achilles_result_cc arc on arc.concept_id = cs.concept_id 
+join @resultSchema.achilles_result_concept_count arc on arc.concept_id = cs.concept_id
 --look only at specific vocabularies that used by our data
 {@includedSourceVocabs !=0} ? {where cs.vocabulary_id in (@includedSourceVocabs)}
 ;
@@ -102,7 +102,7 @@ left join @newVocabSchema.concept c on c.concept_id = cr.concept_id_2
 --3. domain difference
 --old vocabulary vs new, target concept comparison
 create table #resolv_dom_dif  as
-select cohortid, cohortName,conceptsetname, conceptsetid, 
+select cohortid, cohortName,conceptsetname, conceptsetid,
  cn.concept_id , cn.concept_name , cn.vocabulary_id ,
  cs.concept_code as source_concept_code, cs.concept_name as source_concept_name, cs.vocabulary_id as source_vocabulary_id,
  co.domain_id as old_domain_id, cn.domain_id as new_domain_id,
@@ -110,15 +110,17 @@ select cohortid, cohortName,conceptsetname, conceptsetid,
  from (
 select * from #old_vc
 --compare only rows where same included concepts exist
-intersect 
+intersect
 select * from #new_vc
 ) a
 join @oldVocabSchema.concept co on co.concept_id =descendant_concept_id
 join @newVocabSchema.concept cn on cn.concept_id =descendant_concept_id
 --get source concepts related to those targets with changed domains
-join @newVocabSchema.concept_relationship cr on cr.relationship_id ='Mapped from' and cr.concept_id_1 = cn.concept_id 
+join @newVocabSchema.concept_relationship cr on cr.relationship_id ='Mapped from' and cr.concept_id_1 = cn.concept_id
 join @newVocabSchema.concept cs on cs.concept_id = cr.concept_id_2
-left join @resultSchema.achilles_result_cc aro on aro.concept_id = cs.concept_id
+left join @resultSchema.achilles_result_concept_count aro on aro.concept_id = cs.concept_id
 where co.domain_id != cn.domain_id
 {@includedSourceVocabs !=0}? {and cs.vocabulary_id in (@includedSourceVocabs)}
 ;
+--4. cleanup
+drop table if exists @conceptTableName;
